2a8a6bbdc36cac572fb07dd928af5c29a9f54905
Nebojsha Mitikj <nebojsha.97.mitikj@gmail.com>
2025-12-26 00:11:00 +0100

    [main] Add LabelWithTasks type.

--- resources/js/types/labels/Label.d.ts
+++ resources/js/types/labels/Label.d.ts
@@ -1,7 +1,13 @@
-export interface Label {
+export interface LabelBase {
     id: number;
     user_id: number;
     name: string;
     created_at: string;
     updated_at: string;
 }
+
+export interface LabelWithTasks extends LabelBase {
+    tasks: Task[];
+}
+
+export type Label = LabelBase;
