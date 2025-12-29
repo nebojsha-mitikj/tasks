1e62a112e421d0f0d88dbfa83455814176eb0671
Nebojsha Mitikj <nebojsha.97.mitikj@gmail.com>
2025-12-29 11:49:10 +0100

    [main] Add task label list component

---
+++ resources/js/components/tasks/TaskLabels.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { Badge } from '@/components/ui/badge';
+import type { Task } from '@/types/tasks/Task';
+
+const { task } = defineProps<{
+    task: Task;
+}>();
+</script>
+
+<template>
+    <p v-if="task.labels.length > 0" class="text-[11px] text-muted-foreground">
+        Labels:
+        <Badge
+            variant="secondary"
+            v-for="label in task.labels"
+            :key="label.id"
+            class="ml-2 font-bold"
+        >
+            {{ label.name }}
+        </Badge>
+    </p>
+</template>
